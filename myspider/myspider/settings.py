2a34d25a83a6cc22a43f3fde37de8e1d8989dad9
gru <772992382@qq.com>
2016-05-04 16:17:02 +0800

    back-up

---
+++ myspider/myspider/settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for myspider project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'myspider'
+
+SPIDER_MODULES = ['myspider.spiders']
+NEWSPIDER_MODULE = 'myspider.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'myspider (+http://www.yourdomain.com)'
